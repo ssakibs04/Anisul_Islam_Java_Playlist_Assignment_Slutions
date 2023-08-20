574d01128a60ba9487d8e10a3857f1128ec6b720
Sadman Sakib <75657248+ssakibs04@users.noreply.github.com>
2023-08-21 01:41:19 +0600

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sadman.assignment3;
+
+/**
+ *
+ * @author User
+ */
+public class Assignment3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Product.java
@@ -0,0 +1,18 @@
+
+package sadman.assignment2;
+
+
+public class Product {
+    public static void main(String[] args) {
+        int id=101;
+        String title= "iphone15";
+        String price="1895euros";
+        String description="product with best image quality";
+        String category="phone";
+        
+        System.out.println("ID : "+id+"\nTitle : "+title+"\nPrice : "+price+"\nDescription : "+description+"\nCategory : "+category);
+        
+        
+    }
+    
+}

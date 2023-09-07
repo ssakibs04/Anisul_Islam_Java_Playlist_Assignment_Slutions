3ad8dadc681b26f25453f67c77590fc528d1d5b4
Sadman Sakib <75657248+ssakibs04@users.noreply.github.com>
2023-09-07 08:40:21 +0600

    Update Assignment_7logicAndOr.java

--- Assignment_7logicAndOr.java
+++ Assignment_7logicAndOr.java
@@ -1,6 +1,11 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
+// Logical or assignment
+// step 1: Print "Do you love java? "
+// step 2: take user input y / Y / n / N
+// step 3: if user input y / Y then print you are a java lover
+// step 4: if user input n / N then print you are not a java lover
 
 package firsta.assignment_7logicandor;
 
